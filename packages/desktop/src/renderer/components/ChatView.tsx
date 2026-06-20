0cb72f77307bbc744ceab12a2730e364e63d3d5d
vera <l2792069779@gmail.com>
2026-06-21 00:34:47 +0800

    feat(desktop): add ChatView component

---
+++ packages/desktop/src/renderer/components/ChatView.tsx
@@ -0,0 +1,23 @@
+import { Message } from '../api/protocol'
+
+// TODO: Implement MessageList component
+// import { MessageList } from './MessageList'
+
+// TODO: Implement InputBox component
+// import { InputBox } from './InputBox'
+
+interface ChatViewProps {
+  messages: Message[]
+  partialContent?: string
+  isLoading: boolean
+  onSendMessage: (content: string) => void
+}
+
+export function ChatView({ messages, partialContent, isLoading, onSendMessage }: ChatViewProps) {
+  return (
+    <div className="flex flex-col h-full bg-gray-900">
+      {/* <MessageList messages={messages} partialContent={partialContent} /> */}
+      {/* <InputBox isLoading={isLoading} onSendMessage={onSendMessage} /> */}
+    </div>
+  )
+}
